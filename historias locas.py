0a082da1ca38c360b46acc78b0b04b05c0d84334
Paltitaxd <72463857@certus.edu.pe>
2023-04-14 01:08:49 -0500

    Create historias locas.py
    Tarea de python

---
+++ historias locas.py
@@ -0,0 +1,11 @@
+#La F sirve para reemplazar
+
+adj = input("Adjetivo en fem: ")
+verbo1 = input("Verbo: ")
+verbo2 = input("Verbo: ")
+sustantivo_plural = input("Sustantivo (plural en fem): ")
+
+putitas = f"¡Luis es una persona muy {adj}! Siempre le gusta {verbo1} y también {verbo2}.Cuando tiene hambre come muchas {sustantivo_plural}"
+
+print(putitas)
+print("Gracias por probar mi pequeño juego , me merezo un 20")
